918cffafe84db20814657e9d10b826472a7ce45e
Renan <lab7960@caelum133-17>
2018-12-12 18:12:01 -0200

    projeto atualizado

--- src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
@@ -1,7 +1,12 @@
 package br.com.caelum.ingresso.controller;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.DetalhesDoFilme;
 import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.rest.OmdbClient;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -18,58 +25,86 @@ import java.util.Optional;
 @Controller
 public class FilmeController {
 
+	@Autowired
+	private OmdbClient client;
+	
+	@Autowired
+	private FilmeDao filmeDao;
 
-    @Autowired
-    private FilmeDao filmeDao;
+	@Autowired
+	private SessaoDao sessaoDao;
 
+	@GetMapping("/filme/em-cartaz")
+	public ModelAndView emCartaz() {
+		ModelAndView modelAndView = new ModelAndView("filme/em-cartaz");
 
-    @GetMapping({"/admin/filme", "/admin/filme/{id}"})
-    public ModelAndView form(@PathVariable("id") Optional<Integer> id, Filme filme){
+		modelAndView.addObject("filmes", filmeDao.findAll());
 
-        ModelAndView modelAndView = new ModelAndView("filme/filme");
+		return modelAndView;
 
-        if (id.isPresent()){
-            filme = filmeDao.findOne(id.get());
-        }
+	}
 
-        modelAndView.addObject("filme", filme);
+	@GetMapping({ "/admin/filme", "/admin/filme/{id}" })
+	public ModelAndView form(@PathVariable("id") Optional<Integer> id, Filme filme) {
 
-        return modelAndView;
-    }
+		ModelAndView modelAndView = new ModelAndView("filme/filme");
 
+		if (id.isPresent()) {
+			filme = filmeDao.findOne(id.get());
+		}
 
-    @PostMapping("/admin/filme")
-    @Transactional
-    public ModelAndView salva(@Valid Filme filme, BindingResult result){
+		modelAndView.addObject("filme", filme);
 
-        if (result.hasErrors()) {
-            return form(Optional.ofNullable(filme.getId()), filme);
-        }
+		return modelAndView;
+	}
 
-        filmeDao.save(filme);
+	@PostMapping("/admin/filme")
+	@Transactional
+	public ModelAndView salva(@Valid Filme filme, BindingResult result) {
 
-        ModelAndView view = new ModelAndView("redirect:/admin/filmes");
+		if (result.hasErrors()) {
+			return form(Optional.ofNullable(filme.getId()), filme);
+		}
 
-        return view;
-    }
+		filmeDao.save(filme);
 
+		ModelAndView view = new ModelAndView("redirect:/admin/filmes");
 
-    @GetMapping(value="/admin/filmes")
-    public ModelAndView lista(){
+		return view;
+	}
 
-        ModelAndView modelAndView = new ModelAndView("filme/lista");
+	@GetMapping(value = "/admin/filmes")
+	public ModelAndView lista() {
 
-        modelAndView.addObject("filmes", filmeDao.findAll());
+		ModelAndView modelAndView = new ModelAndView("filme/lista");
 
-        return modelAndView;
-    }
+		modelAndView.addObject("filmes", filmeDao.findAll());
 
+		return modelAndView;
+	}
 
-    @DeleteMapping("/admin/filme/{id}")
-    @ResponseBody
-    @Transactional
-    public void delete(@PathVariable("id") Integer id){
-        filmeDao.delete(id);
-    }
+	@GetMapping("/filme/{id}/detalhe")
+	public ModelAndView detalhes(@PathVariable("id") Integer id) {
+		ModelAndView modelAndView = new ModelAndView("/filme/detalhe");
+
+		Filme filme = filmeDao.findOne(id);
+		List<Sessao> sessoes = sessaoDao.buscaSessoesDoFilme(filme);
+		
+		Optional<DetalhesDoFilme> detalhesDoFilme = client.request(filme,DetalhesDoFilme.class);
+		
+
+		modelAndView.addObject("detalhes",detalhesDoFilme.orElse(new DetalhesDoFilme()));
+		modelAndView.addObject("sessoes", sessoes);
+
+		return modelAndView;
+
+	}
+
+	@DeleteMapping("/admin/filme/{id}")
+	@ResponseBody
+	@Transactional
+	public void delete(@PathVariable("id") Integer id) {
+		filmeDao.delete(id);
+	}
 
 }
--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,11 +3,17 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Sessao {
@@ -25,10 +31,13 @@ public class Sessao {
 	
 	private BigDecimal preco;
 	
+	@OneToMany(mappedBy = "sessao", fetch = FetchType.EAGER)
+	private Set<Ingresso> ingressos = new HashSet<>();
+	
 	/**
 	 * @deprecated hibernate only
 	 */
-	Sessao() {
+	public Sessao() {
 		// TODO Auto-generated constructor stub
 	}
 	
@@ -39,6 +48,12 @@ public class Sessao {
 		this.sala = sala;
 		this.preco = sala.getPreco().add(filme.getPreco());
 		
+	}
+	
+	
+	
+	public Map<String, List <Lugar>> getMapaDeLugares(){
+		return sala.getMapaDeLugares();
 	}
 	
 	public BigDecimal getPreco(){
@@ -86,6 +101,8 @@ public class Sessao {
 		this.filme = filme;
 	}
 	
-	
+	public boolean isDisponivel (Lugar lugarSelecionado){
+		return ingressos.stream().map(Ingresso::getLugar).noneMatch(lugar -> lugar.equals(lugarSelecionado));
+	}
 		
 }
